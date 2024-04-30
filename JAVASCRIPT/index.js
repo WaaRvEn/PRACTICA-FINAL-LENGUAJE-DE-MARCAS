39ac76497657f8b24a1fc10f43c89f31a34fd32a
Steven <151622111+WaaRvEn@users.noreply.github.com>
2024-04-30 13:29:32 +0200

    Create index.js

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,26 @@
+// TEMPLATE BOOTSTRAP W3SCHOOL
+
+var mySidebar = document.getElementById("mySidebar");
+
+
+var overlayBg = document.getElementById("myOverlay");
+
+
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+    overlayBg.style.display = "none";
+  } else {
+    mySidebar.style.display = 'block';
+    overlayBg.style.display = "block";
+  }
+}
+
+
+function w3_close() {
+  mySidebar.style.display = "none";
+  overlayBg.style.display = "none";
+}
+
+// STEVEN NATA
+
