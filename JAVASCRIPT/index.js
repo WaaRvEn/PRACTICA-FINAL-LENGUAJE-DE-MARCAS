fc3ba5e39dbdb640feae029ca1fed834f2c23429
Steven <151622111+WaaRvEn@users.noreply.github.com>
2024-05-16 10:39:51 +0200

    Update index.js

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -29,11 +29,10 @@ function añadir(){
   let elemento = document.createElement("p");
 
   let nombre = document.getElementById("form");
-
+  
   elemento.innerHTML = "<br> Bienvenido "+ form.value +". Estas son las reglas: <br> <br> Cada jugador elige un personaje misterioso, y luego haciendo preguntas que tengan como respuesta sí o no, intenta adivinar el personaje misterioso del otro jugador. Cuando piensa que saben quién es el personaje misterioso de su adversario, el jugador puede intentar adivinarlo." ;
 
-  document.getElementById("nombre").appendChild(elemento);
-
+  document.getElementById("nombre").appendChild(elemento); 
 }
 
 function desaparece(k){
@@ -44,15 +43,15 @@ function desaparece(k){
 function elige(){
 
   var imagenes=[
-      '../IMG/Ant-Man.webp',
-      '../IMG/Iron-Man.png',
-      '../IMG/Spider-Man.png',
-      '../IMG/Bruja-Escarlata.png',
-      '../IMG/Vision.png',
-      '../IMG/Cráneo-Rojo.png',
-      '../IMG/Capitan-America.webp',
-      '../IMG/Dr-Strange.webp',
-      '../IMG/Carnage.webp'];
+    'https://static.wikia.nocookie.net/disneyypixar/images/4/4a/Ant-Man.png/revision/latest?cb=20190403174734&path-prefix=es',
+    'https://assets.stickpng.com/thumbs/580b57fbd9996e24bc43c052.png',
+    'https://assets.stickpng.com/thumbs/580b57fbd9996e24bc43c05d.png',
+    'https://assets.stickpng.com/thumbs/580b57fbd9996e24bc43c058.png',
+    'https://assets.stickpng.com/images/580b57fbd9996e24bc43c083.png',
+    'https://images.vectorhq.com/images/previews/4a0/red-skull-psd-440481.png',
+    'https://static.wikia.nocookie.net/disney/images/f/fa/Captain-America-AOU-Render.png/revision/latest?cb=20180420015558&path-prefix=es',
+    'https://static.wikia.nocookie.net/disneyypixar/images/d/de/Dr_Strange.png/revision/latest?cb=20190426034812&path-prefix=es',
+    'https://static.wikia.nocookie.net/death-battle-en-espanol/images/4/4f/Carnage.png/revision/latest?cb=20180725052518&path-prefix=es'];
 
   document.getElementById("tetoca").src = 
   imagenes[Math.floor(Math.random() * imagenes.length)];
